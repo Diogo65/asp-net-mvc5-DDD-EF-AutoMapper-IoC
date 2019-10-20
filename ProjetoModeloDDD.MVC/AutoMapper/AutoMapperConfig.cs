5746b64bd204bb936b52ff71602916ff56956765
Diogo Souza <dsouza@vectoritcgroup.com>
2019-10-20 00:17:16 -0300

    implements interfaces

---
+++ ProjetoModeloDDD.MVC/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace ProjetoModeloDDD.MVC.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToViewModelMappingProfile>();
+            });
+        }
+    }
+}
+
+//O Mapper deve ser registrado dentro do arquivo Global.asax
